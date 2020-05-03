f1647724fb7985fdd2bc1647bdea0e76eadc8ce0
Karan wagh <53109215+FlashTech-dev@users.noreply.github.com>
2020-05-03 23:25:08 +0530

    Update LandingPage.js

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -15,7 +15,7 @@ class Landing extends Component {
   			<h1>Donate for COVID-19</h1>
   			<hr/>
   			<p>Let’s support organisations working on the ground to provide COVID-19 relief and aid! Contribute to any of the fundraisers to support India’s fight against the coronavirus and help the less privileged during this lockdown. </p>
-  			<p>This WebApp is made to ease your access for donating to the genuine organisations all over India. Just login and support the noble cause </p>
+  			<p>This WebApp is made to ease your access for donating to the genuine organisations all over India.Support the noble cause </p>
   			</div>
 
   		</Cell>
