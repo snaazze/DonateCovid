9868d9a7939cf8ac6ade461d5bcdf14e9551a95c
Karan wagh <53109215+FlashTech-dev@users.noreply.github.com>
2020-05-05 00:37:41 +0530

    Create formapp.js

---
+++ src/components/formapp.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Container, Row, Col } from 'reactstrap';
+import Register from './form.js';
+
+function Form() {
+  return (
+    <Container>
+      <Row>
+        <Col md={5}>
+          <Register />
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default form;
