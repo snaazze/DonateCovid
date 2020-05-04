9c959bb82ef3c529158513338779103da42dd527
Karan wagh <53109215+FlashTech-dev@users.noreply.github.com>
2020-05-05 00:39:31 +0530

    Update main.js

--- src/components/main.js
+++ src/components/main.js
@@ -6,6 +6,7 @@ import Pnt from './Pnt.js'
 import Spreads from './Spreads.js'
 import Symptoms from './Symptoms.js'
 import Donation from './Donation.js';
+import Form from './formapp.js';
 const Main =() => (
 	<Switch>
 	<Route exact path="/" component ={LandingPage} />
@@ -14,7 +15,7 @@ const Main =() => (
 	<Route path ="/Pnt" component={Pnt} />
 	<Route path ="/Symptoms" component={Symptoms} />
 	<Route path ="/Donation" component={Donation} />
-
+        <Route path ="/formapp" component={Form}/>
 
 
 	</Switch>
